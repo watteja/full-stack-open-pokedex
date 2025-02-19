74735dfe0f1ac360542b97bf9307deadc429beaf
watteja <mateyast@gmail.com>
2025-02-19 19:14:38 +0100

    Testing failed deployment.

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
 
 app.get('/version', (_req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
