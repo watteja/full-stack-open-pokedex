8584ccf1cdec42df66b1d5df8e526dbcb29a0922
watteja <mateyast@gmail.com>
2025-02-19 18:35:01 +0100

    Exercise 11.10 Deploying your application to Fly.io

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
