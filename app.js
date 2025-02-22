559de87bc05ae4f8649eebd5376216b43a6c3949
watteja <mateyast@gmail.com>
2025-02-22 11:42:17 +0100

    Testing failed build for 11.18

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/version', (_req, res) => {
 })
 
 app.get('/health', (_req, res) => {
-  // // eslint-disable-next-line no-constant-condition
-  // if (true) throw('error...  ')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
